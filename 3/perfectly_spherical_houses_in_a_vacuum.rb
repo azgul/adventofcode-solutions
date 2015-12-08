f9b0b4784a6f40e88487841c4de36742323df7c0
Martin <me@azgul.com>
2015-12-08 21:58:40 +0100

    day 3: part 2 solution - deliver gifts cleverly

--- 3/perfectly_spherical_houses_in_a_vacuum.rb
+++ 3/perfectly_spherical_houses_in_a_vacuum.rb
@@ -44,6 +44,42 @@ end
 
 TESTS_PART_ONE.each { |test| raise test[:assertion] unless (deliver_gifts(test[:steps]).size == test[:gifted_houses]) }
 
-gifted_houses = deliver_gifts(File.read('input')).size
+TESTS_PART_TWO = [
+  {
+    assertion: '^v delivers presents to 3 houses, because Santa goes north, and then Robo-Santa goes south.',
+    steps: '^v',
+    gifted_houses: 3
+  },
+  {
+    assertion: '^>v< now delivers presents to 3 houses, and Santa and Robo-Santa end up back where they started.',
+    steps: '^>v<',
+    gifted_houses: 3
+  },
+  {
+    assertion: '^v^v^v^v^v now delivers presents to 11 houses, with Santa going one direction and Robo-Santa going the other.',
+    steps: '^v^v^v^v^v',
+    gifted_houses: 11
+  }
+]
 
+def deliver_gift_cleverly(steps)
+  santa_steps = ""
+  robo_steps = ""
+
+  steps.each_char do |chr|
+    if santa_steps.length == robo_steps.length
+      santa_steps << chr
+    else
+      robo_steps << chr
+    end
+  end
+
+  (deliver_gifts(santa_steps) + deliver_gifts(robo_steps)).uniq
+end
+
+TESTS_PART_TWO.each { |test| raise test[:assertion] unless (deliver_gift_cleverly(test[:steps]).size == test[:gifted_houses]) }
+
+gifted_houses = deliver_gifts(File.read('input')).size
+cleverly_gifted_houses = deliver_gift_cleverly(File.read('input')).size
 puts "Gifted houses: #{gifted_houses}"
+puts "Cleverly gifted houses: #{cleverly_gifted_houses}"
