6c2d2d29fb128e5fb3b7a90c587728e7a6fd7828
Martin <me@azgul.com>
2015-12-10 21:31:26 +0100

    day 5: part 2 solution

--- 5/doesnt_he_have_intern_elves_for_this.rb
+++ 5/doesnt_he_have_intern_elves_for_this.rb
@@ -52,3 +52,53 @@ nice_strings = 0
 input.each { |str| nice_strings += 1 if is_string_nice(str) }
 
 puts "Nice strings #{nice_strings}"
+
+TESTS_PART_TWO = [
+  {
+    assertion: 'qjhvhtzxzqqjkmpb is nice because is has a pair that appears twice (qj) and a letter that repeats with exactly one letter between them (zxz).',
+    string: 'qjhvhtzxzqqjkmpb',
+    nice: true
+  },
+  {
+    assertion: 'xxyxx is nice because it has a pair that appears twice and a letter that repeats with one between, even though the letters used by each rule overlap.',
+    string: 'xxyxx',
+    nice: true
+  },
+  {
+    assertion: 'uurcxstgmygtbstg is naughty because it has a pair (tg) but no repeat with a single letter between them.',
+    string: 'uurcxstgmygtbstg',
+    nice: false
+  },
+  {
+    assertion: 'ieodomkazucvgmuy is naughty because it has a repeating letter with one between (odo), but no pair that appears twice.',
+    string: 'ieodomkazucvgmuy',
+    nice: false
+  }
+]
+
+def is_string_really_nice(str)
+  chars = str.chars
+
+  contains_repeat_letter = false
+  substring_in_remainder = false
+  for i in 0..(str.length - 2) do
+    pair = str.slice(i, 2)
+    rest = str.slice(i + 2, str.length - i)
+
+    contains_repeat_letter = true if str[i] == str[i + 2] && str[i] != str[i + 1]
+
+    if (!rest.nil? && rest.include?(pair) && !substring_in_remainder)
+      substring_in_remainder = true
+    end
+  end
+
+  return substring_in_remainder && contains_repeat_letter
+end
+
+TESTS_PART_TWO.each { |test| fail test[:assertion] unless is_string_really_nice(test[:string]) == test[:nice] }
+
+input = File.read('input').lines
+really_nice_strings = 0
+input.each { |str| really_nice_strings += 1 if is_string_really_nice(str) }
+
+puts "Really nice strings #{really_nice_strings}"
